a1323ff72996b7ef1856bb9bcb5a35df7f62ff61
Sejmou <samo.kolter@gmail.com>
2022-02-20 15:41:22 +0100

    add data exploration helper scripts

--- test.py
+++ test.py
@@ -1,72 +1,19 @@
-from helper_scripts.data_loaders import CIFAR10Loader, GTSRBLoader
 import matplotlib.pyplot as plt
-import numpy as np
 
-def show_img(img_arr, ax=None):
-  grayscale = img_arr.ndim == 2
-  if ax:
-    if grayscale:
-      ax.imshow(img_arr, cmap='gray', vmin=0, vmax=255)#https://stackoverflow.com/a/3823822/13727176
-    else: 
-      ax.imshow(img_arr)
-  else:
-    if grayscale:
-      plt.imshow(img_arr, cmap='gray', vmin=0, vmax=255)
-    else: 
-      plt.imshow(img_arr)
+from helper_scripts.data_loaders import CIFAR10Loader, GTSRBLoader
+from helper_scripts.data_exploration import print_dataset_summary, show_img, index_to_data_part
 
-def check_loaded_data(data, dataset_name):
-  print(f'\n--- Checking loaded {dataset_name} data ---')
-  print('data types')
-  for val in data:
-    print(type(val))
-  print()
+CIFAR10_data = CIFAR10Loader('./data').get_processed_imgs(42, 42, normalize=False, convert_to_grayscale=True)
+GTSRB_data = GTSRBLoader('./data/GTSRB').get_processed_imgs(42, 42, normalize=False, convert_to_grayscale=True)
 
-  print('data shapes')
-  for val in data:
-    shape = getattr(val, 'shape', None)
-    if not (shape == None): print(val.shape)
-    else: print(f'Warning: Expected property "shape", but it was not present!')
-  print()
+print_dataset_summary(CIFAR10_data, 'CIFAR-10')
+print_dataset_summary(GTSRB_data, 'GTSRB')
 
-  all_imgs = np.concatenate(data[:3])
-  print('Image stats:')
-  print(f'shape: {all_imgs.shape}')
-  print(f'Max value across all images and channels: {all_imgs.max()}')
-  print(f'Min value across all images and channels: {all_imgs.min()}')
 
-  split_sizes = [get_length(data_split) for data_split in data[:3]]
-  train_size, val_size, test_size = split_sizes
-  total_size = sum(split_sizes)
-  print('split sizes:')
-  print(f'{train_size=}, {val_size=}, {test_size=}')
-  print(f'Total dataset size: {total_size}')
-  print()
-
-  split_ratios = [get_ratio_rounded(split_size, total_size) for split_size in split_sizes]
-  print(f'Train/Val/Test ratio: {"/".join(str(split) for split in split_ratios)}')
-  train_and_val_size = train_size + val_size
-  print('Train/Val ratio:' + f"{get_ratio_rounded(train_size, train_and_val_size)}/{get_ratio_rounded(val_size, train_and_val_size)}")
-  print('(Train+Val)/Test ratio:' + f"{get_ratio_rounded(train_and_val_size, total_size)}/{get_ratio_rounded(test_size, total_size)}")
-
-  print()
-
-index_to_data_part = {
-  0: 'X_train',
-  1: 'X_val',
-  2: 'X_test',
-  3: 'y_train',
-  4: 'y_val',
-  5: 'y_test'
-}
-
-def get_length(data):
-  shape = getattr(data, 'shape', None)
-  if not (shape == None): return data.shape[0]
-  else: return len(data)
-
-def get_ratio_rounded(part, total):
-  return round(part/total, 2)
+# check if first three return values of both datasets are valid image arrays
+# we simply try to plot first image of X_train, X_val, and X_test
+fig, ax = plt.subplots(2, 3)
+ax = ax.flat
 
 def plot_first_img_from_data(data, data_name, ax=None):
   print(f'loading {data_name}')
@@ -76,21 +23,6 @@ def plot_first_img_from_data(data, data_name, ax=None):
   print('\nloading first image')
   print(f'shape: {img_data.shape}\n')
   show_img(img_data, ax=ax)
-
-CIFAR10_data = CIFAR10Loader('./data').get_processed_imgs(42, 42, normalize=False, convert_to_grayscale=True)
-
-check_loaded_data(CIFAR10_data, 'CIFAR-10')
-
-
-GTSRB_data = GTSRBLoader('./data/GTSRB').get_processed_imgs(42, 42, normalize=False, convert_to_grayscale=True)
-
-check_loaded_data(GTSRB_data, 'GTSRB')
-
-
-# check if first three return values of both datasets are valid image arrays
-# we simply try to plot first image of X_train, X_val, and X_test
-fig, ax = plt.subplots(2, 3)
-ax = ax.flat
 
 print('\nPlotting example images from CIFAR-10')
 for i in range(3):
